f1b535dfcb4dc5c0c0e2ab72a36ce3792e8cfacc
ShuTing WU <shuwu@alu.edu.gva.es>
2026-02-17 23:27:25 +0100

    fix: add missing getter for foto1

--- src/main/java/com/emergencias/data/CentroSalud.java
+++ src/main/java/com/emergencias/data/CentroSalud.java
@@ -3,19 +3,71 @@ package com.emergencias.data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CentroSalud {
-  @JsonProperty("Código") public String codigo;
-  @JsonProperty("Nombre") public String nombre;
-  @JsonProperty("Dirección") public String direccion;
-  @JsonProperty("C.P.") public String cp;
-  @JsonProperty("Municipio") public String municipio;
-  @JsonProperty("Pedanía") public String pedania;
-  @JsonProperty("Teléfono") public String telefono;
-  @JsonProperty("Fax") public String fax;
-  @JsonProperty("Email") public String email;
-  @JsonProperty("URL Real") public String urlReal;
-  @JsonProperty("URL Corta") public String urlCorta;
-  @JsonProperty("Latitud") public String latitud;
-  @JsonProperty("Longitud") public String longitud;
-  @JsonProperty("Foto 1") public String foto1;
-  @JsonProperty("Foto 2") public String foto2;
+
+    @JsonProperty("Código")
+    private String codigo;
+
+    @JsonProperty("Nombre")
+    private String nombre;
+
+    @JsonProperty("Dirección")
+    private String direccion;
+
+    @JsonProperty("C.P.")
+    private String cp;
+
+    @JsonProperty("Municipio")
+    private String municipio;
+
+    @JsonProperty("Pedanía")
+    private String pedania;
+
+    @JsonProperty("Teléfono")
+    private String telefono;
+
+    @JsonProperty("Fax")
+    private String fax;
+
+    @JsonProperty("Email")
+    private String email;
+
+    @JsonProperty("URL Real")
+    private String urlReal;
+
+    @JsonProperty("URL Corta")
+    private String urlCorta;
+
+    @JsonProperty("Latitud")
+    private String latitud;
+
+    @JsonProperty("Longitud")
+    private String longitud;
+
+    @JsonProperty("Foto 1")
+    private String foto1;
+
+    @JsonProperty("Foto 2")
+    private String foto2;
+
+    public CentroSalud() {
+    }
+
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public String getDireccion() { return direccion; }
+    public String getMunicipio() { return municipio; }
+    public String getTelefono() { return telefono; }
+    public String getFoto1() { return foto1; }
+    public void setFoto1(String foto1) { this.foto1 = foto1; }
+
+
+    @Override
+    public String toString() {
+        return "CentroSalud{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", municipio='" + municipio + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
 }
